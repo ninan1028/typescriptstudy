1bf7a8ad8696c96f75ebd35401dec2f2b280f8d6
wangzhenpeng <814263832@qq.com>
2019-03-29 21:11:13 +0800

    typescript 1

---
+++ typeScriptStydy/day1/eg1.js
@@ -0,0 +1,7 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "Jane User";
+var userarr = [1, 2];
+console.log(greeter(user));
+console.log(greeter(userarr));
---
+++ typeScriptStydy/day1/eg1.ts
@@ -0,0 +1,12 @@
+
+function greeter(person:string) {
+    return "Hello, " + person;
+}
+
+let user = "Jane User";
+
+let  userarr=[1,2];
+
+console.log(greeter(user));
+
+console.log(greeter(userarr));
